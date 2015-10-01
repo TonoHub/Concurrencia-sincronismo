d36b289b67f90db871879a553fbd6628dda3c6f5
tono <tonollado@hotmail.com>
2015-10-01 20:21:10 +0200

    Initial commit

---
+++ src/com/company/Consumer.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * Created by tono on 28/09/2015.
+ */
+public class Consumer implements Runnable {
+    private Buffer buffer = null;
+
+    public Consumer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        StringBuilder sb = new StringBuilder();
+        char c;
+        while ((c = buffer.get()) != '\n') {
+            // Uncomment to log what is reading the thread
+            // System.out.println(Thread.currentThread().getName() + " reads " + c);
+            sb.append(c);
+        }
+        System.out.println(sb);
+    }
+}
